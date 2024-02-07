42013841c8dab5e1825c580e7ff7d9acecb93e96
dean10042008 <dean.yurukov2008@gmail.com>
2024-02-07 13:49:38 +0200

    13.Exam-Preparation-part-2

--- Programming Basics with JavaScript January 2024/13.Exam-Preparation/06.Easter-Competition/index.js
+++ Programming Basics with JavaScript January 2024/13.Exam-Preparation/06.Easter-Competition/index.js
@@ -1,3 +1,7 @@
 function competition(input) {
     
 }
+
+competition(["3", "Chef Manchev", "10", "10", "10", "10", "Stop", "Natalie", "8", "2", "9", "Stop", "George", "9", "2", "4", "2", "Stop"]);
+
+competition(["2", "Chef Angelov", "9", "9", "9", "Stop", "Chef Rowe", "10", "10", "10", "10", "Stop"]);
