b3646b63d9169fe60d7eec7b68707387526a3146
dean10042008 <dean.yurukov2008@gmail.com>
2024-02-11 15:50:10 +0200

    exam-prep-part-3

--- Programming Basics with JavaScript January 2024/13.Exam-Preparation/06.Easter-Competition/index.js
+++ Programming Basics with JavaScript January 2024/13.Exam-Preparation/06.Easter-Competition/index.js
@@ -1,7 +1,33 @@
-function competition(input) {
-    
+function easterCompetition(input) {
+    let currentLine = 0;
+    function readLine() {
+        return input[currentLine++];
+    }
+
+    let count = parseInt(readLine());
+    let bestBakerName = "";
+    let bestBakerPoints = -1;
+
+    for (let i = 0; i < count; i++) {
+        let bakerName = readLine();
+        let points = 0;
+        let command = readLine();
+
+        while (command !== "Stop") {
+            points += parseInt(command);
+            command = readLine();
+        }
+
+        console.log(`${bakerName} has ${points} points.`);
+
+        if (points > bestBakerPoints) {
+            bestBakerName = bakerName;
+            bestBakerPoints = points;
+            console.log(`${bakerName} is the new number 1!`);
+        }
+    }
+
+    console.log(`${bestBakerName} won competition with ${bestBakerPoints} points!`);
 }
 
-competition(["3", "Chef Manchev", "10", "10", "10", "10", "Stop", "Natalie", "8", "2", "9", "Stop", "George", "9", "2", "4", "2", "Stop"]);
-
-competition(["2", "Chef Angelov", "9", "9", "9", "Stop", "Chef Rowe", "10", "10", "10", "10", "Stop"]);
+easterCompetition(["3", "Chef Manchev", "10", "10", "10", "10", "Stop", "Natalie", "8", "2", "9", "Stop", "George", "9", "2", "4", "2", "Stop"]);
