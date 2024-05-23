7cf8d4cd0cda4165e0abf55915592dc9d5813671
dean10042008 <dean.yurukov2008@gmail.com>
2024-05-23 19:08:52 +0300

    Deleting course

--- JS Front-End October 2023/01.JS-Syntax-Fundamentals-Lab/01.Multiply the Number by 2.js
+++
@@ -1,6 +0,0 @@
-function solve (num) {
-    console.log(num * 2);
-}
-solve(2);
-solve(5);
-solve(10);
--- JS Front-End October 2023/01.JS-Syntax-Fundamentals-Lab/10.Numbers-from-M-to-N.js
+++
@@ -1,8 +0,0 @@
-function solve (m, n) {
-    for (let i=m; i>n; i--) {
-        console.log(i);
-    }
-}
-
-solve(6, 2);
-solve(4, 1);
