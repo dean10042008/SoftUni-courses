74e8d245956ee61a957081446fe610e2af67a6e4
dean10042008 <dean.yurukov2008@gmail.com>
2024-06-29 14:07:36 +0300

    Changes-to-16.Mid-Exam-Prep-2

---
+++ Programming Fundamentals with JavaScript May 2024/16.Mid-Exam-Prep-2/03.Digits/index.js
@@ -0,0 +1,30 @@
+function digits(input) {
+    let nums = input.split(' ').map(Number);
+
+    let sum = 0;
+    let result = [];
+
+    for (const num of nums) {
+        sum += num;
+    }
+
+    const average = sum / nums.length;
+    nums.sort((a, b) => b - a);
+    let count = 1;
+
+    for (const num of nums) {
+        if (num > average && count <= 5) {
+            count++;
+            result.push(num);
+        }
+    }
+
+    if (count === 1) {
+        console.log("No");
+        return;
+    }
+
+    console.log(result.join(" "));
+}
+
+digits('10 20 30 40 50');
