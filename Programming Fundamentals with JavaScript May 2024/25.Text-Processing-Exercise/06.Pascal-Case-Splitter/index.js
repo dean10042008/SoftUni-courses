f2c1761653d75cf39969b7fcf3d7b54b9f8c9c5b
dean10042008 <dean.yurukov2008@gmail.com>
2024-07-11 21:07:59 +0300

    Fix 25.Text-Processing-Exercise/06.Pascal-Case-Splitter

--- Programming Fundamentals with JavaScript May 2024/25.Text-Processing-Exercise/06.Pascal-Case-Splitter/index.js
+++ Programming Fundamentals with JavaScript May 2024/25.Text-Processing-Exercise/06.Pascal-Case-Splitter/index.js
@@ -1,6 +1,37 @@
 function pascalCaseSplitter(str) {
-    let words = str.split(/(?=[A-Z])/);
-    console.log(words.join(', '));
+    let arr = str.split('');
+    let result = [];
+
+    while (arr.length !== 0) {
+        let word = arr[0];
+        let codeWord = word.charCodeAt(0);
+
+        if (codeWord >= 65 && codeWord <= 90) {
+            let j = 1;
+
+            while (arr.length !== 0) {
+                let nextSymbol = arr[j];
+
+                if (nextSymbol === undefined) {
+                    result.push((arr.splice(0)).join(""));
+                }
+                else {
+                    let codeSymbol = nextSymbol.charCodeAt(0);
+
+                    if (codeSymbol >= 65 && codeSymbol <= 90) {
+                        let spliced = arr.splice(0, j);
+                        result.push(spliced.join(""));
+                        j = 1;
+                    }
+                    else {
+                        j++;
+                    }
+                }
+            }
+        }
+    }
+
+    console.log(result.join(", "));
 }
 
 pascalCaseSplitter('SplitMeIfYouCanHaHaYouCantOrYouCan');
