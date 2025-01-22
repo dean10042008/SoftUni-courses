10e5a855f6a314b1388f66f0c6b4eb1955105253
dean10042008 <dean.yurukov2008@gmail.com>
2025-01-22 21:55:14 +0200

    06.Workshop-NoSQL-and-MongoDB

---
+++ JS Back-End January 2025/06.Workshop-NoSQL-and-MongoDB/01.Movie-Magic-Workshop-Part-2/src/index.js
@@ -0,0 +1,174 @@
+import express from 'express';
+import handlebars from 'express-handlebars';
+import mongoose from 'mongoose';
+
+import Movie from './models/Movie.js';
+
+import { getAll } from './services/movie-service.js';
+
+
+const uriMovies = 'mongodb://0.0.0.0:27017/Movies';
+
+try {
+    await mongoose.connect(uriMovies);
+    console.log('Connected to DB Successfully');
+} catch (err) {
+    console.error('Cannot connect to DB!');
+    console.log(err.message);
+}
+
+const app = express();
+
+// const movies = [
+//     {
+//         id: "a3682672-0ee4-1284-8759-35ee253329zv",
+//         title: 'Jungle Cuise',
+//         genre: 'Adventure',
+//         description: 'Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.',
+//         imageUrl: '/img/jungle-cruise.jpeg',
+//         director: 'Jaume Collet-Serra',
+//         year: '2021',
+//         rating: 6.6,
+//         category: 'movie',
+//     },
+//     {
+//         id: "z2682672-0ee4-1534-8759-35ee253329ty",
+//         title: 'Man of Steel',
+//         genre: 'Superhero',
+//         description: 'An alien child is evacuated from his dying world and sent to Earth to live among humans. His peace is threatened when other survivors of his home planet invade Earth.',
+//         imageUrl: 'https://m.media-amazon.com/images/M/MV5BMTk5ODk1NDkxMF5BMl5BanBnXkFtZTcwNTA5OTY0OQ@@._V1_FMjpg_UX1000_.jpg',
+//         director: 'Zack Snyder',
+//         year: '2013',
+//         rating: 7.1,
+//         category: 'movie',
+//     },
+//     {
+//         id: '81313c94-08e0-40bf-85bc-1e7cdeebbef9',
+//         title: 'Avengers: Endgame',
+//         category: 'movie',
+//         genre: 'Superhero',
+//         director: 'Anthony Russo',
+//         year: '2019',
+//         imageUrl: 'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg',
+//         rating: 8.4,
+//         description: "After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe."
+//     }
+// ];
+
+// const newMovie = new Movie({
+//     id: "z2682672-0ee4-1534-8759-35ee253329ty",
+//     title: 'Man of Steel',
+//     genre: 'Superhero',
+//     description: 'An alien child is evacuated from his dying world and sent to Earth to live among humans. His peace is threatened when other survivors of his home planet invade Earth.',
+//     imageUrl: 'https://m.media-amazon.com/images/M/MV5BMTk5ODk1NDkxMF5BMl5BanBnXkFtZTcwNTA5OTY0OQ@@._V1_FMjpg_UX1000_.jpg',
+//     director: 'Zack Snyder',
+//     year: '2013',
+//     rating: 7.1,
+//     category: 'movie',
+// });
+
+// await newMovie.save();
+
+// Config static middleware
+
+app.use(express.static('../public'));
+app.use(express.urlencoded({ extended: false }));
+
+// Add handlebars engine to express engines
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+        allowedProtoMethodsByDefault: true
+    }
+}));
+
+// Set default engine
+app.set('view engine', 'hbs');
+
+app.get('/', async (req, res) => {
+    res.render("home", { movies: await getAll() });
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.get("/movies/:id/details", async (req, res) => {
+    const { id } = req.params;
+    const movie = await Movie.findById(id);
+    res.render("details", { movie });
+});
+
+app.get("/createMovie", (req, res) => {
+    res.render("create-movie");
+});
+
+app.post("/createMovie", async (req, res) => {
+    let data = "";
+
+    req.on("data", (chunk) => {
+        data += chunk;
+    });
+
+
+    req.on("end", async () => {
+        const { title, genre, description, imageUrl, director, year, rating, category } = JSON.parse(data);
+
+        const newMovie = {
+            title,
+            genre,
+            description,
+            imageUrl,
+            director,
+            year: Number(year),
+            rating: Number(rating),
+            category,
+        };
+
+        try {
+            await Movie.create(newMovie);
+        }
+        catch (err) {
+            console.error(err);
+            res.status(400).send({ message: "Invalid movie data." });
+        }
+        finally {
+            return res.end();
+        }
+    });
+});
+
+app.get('/createCast', (req, res) => {
+    res.render('create-cast');
+});
+
+app.get("/search", async (req, res) => {
+    res.render("search", { movies: await getAll() });
+});
+
+app.post("/search", async (req, res) => {
+    const searchParams = req.body;
+
+    let query = {};
+
+    if (searchParams.title) {
+        query.title = new RegExp(searchParams.title, "i");
+    }
+    if (searchParams.genre) {
+        query.genre = new RegExp(searchParams.genre, "i");
+    }
+    if (searchParams.year) {
+        query.year = searchParams.year;
+    }
+
+    let filteredMovies = await Movie.find(query);
+
+    res.render("search", { movies: filteredMovies, filter: searchParams });
+});
+
+app.all("*", (req, res) => {
+    res.render("404");
+});
+
+app.listen(5001, () => console.log("App is listening on http://localhost:5001..."));
